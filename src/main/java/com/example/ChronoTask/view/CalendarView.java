d0b76bb1832803b8aca4a2fb47c31095e776dd09
VasiVladimir <prodoge227@gmail.com>
2025-05-18 15:21:21 +0200

    Испраил ошибку

--- src/main/java/com/example/ChronoTask/view/CalendarView.java
+++ src/main/java/com/example/ChronoTask/view/CalendarView.java
@@ -31,7 +31,6 @@ import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import static com.vaadin.uitest.parser.Parser.objectMapper;
 
 @Route("calendar")
 @AnonymousAllowed
